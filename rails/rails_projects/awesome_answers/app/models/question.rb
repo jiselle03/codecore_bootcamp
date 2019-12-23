a02e5c23365d7b28095cc4451045c202afd7a3aa
Jiselle Liu <jiselle03@gmail.com>
2019-12-23 12:00:54 -0800

    Update rails project for Rails Active Record class

---
+++ rails/rails_projects/awesome_answers/app/models/question.rb
@@ -0,0 +1,47 @@
+class Question < ApplicationRecord
+    # Question model
+    # Generated with command 'rails g model question title:string body:text'
+    # Question class inherits everything from ApplicationRecord
+
+    # ActiveRecord is an ORM
+    # It provides a bunch of methods to query the database
+
+    # Rails HOOK
+    before_save :capitalize_title
+    # Before saving a record, execute the method
+
+    # Validations
+    # Rails has built-in methods which allow us to create validations easily
+    validates :title, presence: true # will make sure a question has a title before saving it
+
+    # validate uniqueness of title
+    validates :title, uniqueness: true
+
+    # validate length of body
+    validates :body, length: {minimum: 5, maximum: 500}
+
+    # view count must be at least 0
+    validates :view_count, numericality: {greater_than_or_equal_to: 0}
+
+    # Custom validation
+    validate(:no_monkey)
+
+    private
+
+    def capitalize_title
+        self.title.capitalize!
+    end
+
+    def no_monkey
+        # '&.' is the safe navigator operator. It is used like the '.' operator to call
+        # methods on an object only if they exist
+        if body&.downcase&.include?("monkey")
+            # If a body of a question has the word "monkey" in it, give us an error
+            # 'errors.add' is used to add an error to the instance. It accepts 2 arguments:
+                # 1) the column/property you want the error to be on
+                # 2) the error message
+            self.errors.add(:body, "must not have monkeys")
+        end
+    end
+
+end
--- rails/rails_projects/awesome_answers/db/seeds.rb
+++ rails/rails_projects/awesome_answers/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.destroy_all
+
+200.times do
+    Question.create(
+        title: Faker::Hacker.say_something_smart,
+        body: Faker::ChuckNorris.fact,
+        view_count: rand(100_000),
+        created_at: Faker::Date.backward(days:365 * 5),
+        updated_at: Faker::Date.backward(days:365 * 5)
+    )
+end
+
+puts Cowsay.say("Generated #{Question.count} questions", :frogs)
