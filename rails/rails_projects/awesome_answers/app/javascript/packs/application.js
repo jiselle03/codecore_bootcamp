a8be2eb381ad71a6d85011c7c5dc45575de55ed2
Jiselle Liu <jiselle03@gmail.com>
2020-01-08 13:06:06 -0800

    Update rails project for Rails: Authorization, Asset Pipeline & Heroku class

--- rails/rails_projects/awesome_answers/app/javascript/packs/application.js
+++ rails/rails_projects/awesome_answers/app/javascript/packs/application.js
@@ -7,6 +7,12 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'bootstrap'
+import './questions'
+
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
