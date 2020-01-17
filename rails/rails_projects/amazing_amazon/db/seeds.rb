6e5a82f8168f9191f859ba58a524616b43a261a2
Jiselle Liu <jiselle03@gmail.com>
2020-01-17 12:30:27 -0800

    Update seed file to add votes, favorites, likes

--- rails/rails_projects/amazing_amazon/db/seeds.rb
+++ rails/rails_projects/amazing_amazon/db/seeds.rb
@@ -10,7 +10,9 @@ Product.destroy_all()
 Review.destroy_all()
 User.destroy_all()
 Favorite.destroy_all()
+Like.destroy_all()
 Tag.destroy_all()
+Vote.destroy_all()
 
 PASSWORD = "supersecret"  
 
@@ -22,7 +24,7 @@ super_user = User.create(
     is_admin: true
 ) 
 
-100.times do 
+50.times do 
     first_name = Faker::Name.first_name 
     last_name = Faker::Name.last_name 
     User.create( 
@@ -34,7 +36,7 @@ super_user = User.create(
 end 
 
 users = User.all 
-puts Cowsay.say("Created #{users.count} users.", :tux)  
+puts Cowsay.say("Created #{users.count} users.", :frogs)  
 puts "Login with #{super_user.email} and password of '#{PASSWORD}'."
 
 10.times do
@@ -46,24 +48,41 @@ end
 tags = Tag.all
 
 250.times do
-    user = users.sample 
+    random_date = Faker::Date.backward(days:365 * 5)
     p = Product.create(
         title: Faker::Commerce.product_name,
         description: Faker::Movies::StarWars.quote,
         price: Faker::Number.decimal(l_digits: 2),
-        created_at: Faker::Date.backward(days:365 * 5),
-        updated_at: Faker::Date.backward(days:365 * 5),
-        user_id: user.id
+        created_at: random_date,
+        updated_at: random_date,
+        user: users.sample
     )
     if p.valid?
-        p.reviews = rand(0..15).times.map do
-            user = users.sample
-            Review.new(body: Faker::Movies::StarWars.wookiee_sentence, rating: Faker::Number.within(range: 1..5), user_id: user.id)
+        rand(0..15).times.map do
+            r = Review.create(
+                body: Faker::Movies::StarWars.wookiee_sentence, 
+                rating: Faker::Number.within(range: 1..5), 
+                product: p,
+                user: users.sample
+                )
+            p.fans = users.shuffle.slice(0..rand(users.count))
+            p.tags = tags.shuffle.slice(0..rand(tags.count))
+            if r.valid?
+                r.likers = users.shuffle.slice(0..rand(users.count))
+                users.shuffle.slice(0..rand(users.count)).each do |user|
+                    Vote.create(
+                        review: r,
+                        user: user,
+                        is_up: [true, false].sample
+                    )
+                end
+            end
         end
-        p.fans = users.shuffle.slice(0, rand(users.count))
-        p.tags = tags.shuffle.slice(0, rand(tags.count))
     end
 end
 
 puts Cowsay.say("Generated #{Product.count} products and #{Review.count} reviews.", :dragon)
-puts Cowsay.say("Generated #{Tag.count} tags.", :frogs)
+puts Cowsay.say("Generated #{Favorite.count} favorites.", :tux)
+puts Cowsay.say("Generated #{Like.count} likes.", :tux)
+puts Cowsay.say("Generated #{Tag.count} tags.", :tux)
+puts Cowsay.say("Generated #{Vote.count} votes.", :tux)
