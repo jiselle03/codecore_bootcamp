eefc917294574c11f442089bfa8fae5b15aac65b
Jiselle Liu <jiselle03@gmail.com>
2020-01-20 00:00:34 -0800

    Remove manual routes - method DELETE/POST/PATCH works without manual GET routes when cloned from github

--- rails/rails_projects/pm_tool/config/routes.rb
+++ rails/rails_projects/pm_tool/config/routes.rb
@@ -13,17 +13,7 @@ Rails.application.routes.draw do
   get('/users/:id/password/edit', { to: 'users#edit_password', as: 'edit_password' })
   patch('/users/:id/password/edit', { to: 'users#update_password', as: 'update_password'})
   
-  get("/projects/:id/destroy", { to: "projects#destroy", as: :project_destroy })
-  get("/projects/:project_id/tasks", { to: "tasks#create", as: :tasks_create })
-  get("/tasks/:id", { to: "tasks#destroy", as: :task_destroy })
-  get("/tasks/:id/edit", { to: "tasks#update", as: :task_update })
-  
-  get("/projects/:project_id/favorites", { to: "favorites#create", as: :favorites_create })
-  get("/favorites/:id", { to: "favorites#destroy", as: :favorites_destroy })
-  
   resources :users, only: [:new, :create, :edit, :update]
   resource :session, only: [:new, :create, :destroy]
-  
-  get("/session/destroy", { to: "sessions#destroy", as: :session_destroy })
   
 end
