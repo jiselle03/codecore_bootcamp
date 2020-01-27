02f282238e4ca063f4530ba59588471c9663d1a2
Jiselle Liu <jiselle03@gmail.com>
2020-01-27 10:23:09 -0800

    Add controller for questions api v1

---
+++ rails/rails_projects/awesome_answers/app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::QuestionsController < ApplicationController
+    def index
+        questions = Question.order(created_at: desc)
+        render json: questions
+    end
+
+    def show
+        question = Question.find(params[:id])
+        render json: question
+    end
+end
