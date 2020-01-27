821068c123ae06f90b9d3b47c0232011aa8ab3f4
Jiselle Liu <jiselle03@gmail.com>
2020-01-27 13:48:09 -0800

    Add show for products controller api version

--- rails/rails_projects/amazing_amazon/app/controllers/api/v1/products_controller.rb
+++ rails/rails_projects/amazing_amazon/app/controllers/api/v1/products_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::ProductsController < ApplicationController
         products = Product.order(created_at: :desc)
         render json: products
     end
+
+    def show
+        product = Product.find(params[:id])
+        render json: product
+    end
 end
