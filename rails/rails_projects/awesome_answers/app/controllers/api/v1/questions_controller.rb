5dc5d5153de5bc8f5315e62e81ad1c33a9dfbbd9
Jiselle Liu <jiselle03@gmail.com>
2020-01-27 11:53:16 -0800

    Make it inherit from api application controller

--- rails/rails_projects/awesome_answers/app/controllers/api/v1/questions_controller.rb
+++ rails/rails_projects/awesome_answers/app/controllers/api/v1/questions_controller.rb
@@ -1,11 +1,35 @@
-class Api::V1::QuestionsController < ApplicationController
+class Api::V1::QuestionsController < Api::ApplicationController
+
+    def create
+        question = Question.new question_params
+        question.user = current_user
+        if question.save
+            render json: { id: question.id }
+        else
+            render(
+                json: { errors: question.errors },
+                status: 422 #unproceesable entity
+            )
+        end
+    end
+
     def index
-        questions = Question.order(created_at: desc)
-        render json: questions
+        questions = Question.order(created_at: :desc)
+        # This allows us to use the QuestionCollectionSerializer to
+        # render json of questions in this list in order to keep the data 
+        # limited to the minimum we need.
+        render json: questions, each_serializer: QuestionCollectionSerializer
     end
 
     def show
         question = Question.find(params[:id])
-        render json: question, include: ['answers.author']
+        render json: question#, include: ['answers.author']
     end
+
+    private
+
+    def question_params
+        params.require(:question).permit(:title, :body)
+    end
+
 end
