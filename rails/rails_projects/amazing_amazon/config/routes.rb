e2ba9ec696062c65f62fdee2bab59a5f16b70080
Jiselle Liu <jiselle03@gmail.com>
2020-01-27 14:01:19 -0800

    Add route for sessions

--- rails/rails_projects/amazing_amazon/config/routes.rb
+++ rails/rails_projects/amazing_amazon/config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do 
       resources :products
+      resource :session, only: [:create, :destroy]
     end
   end
 
