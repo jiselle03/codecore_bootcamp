8f089fb6ccb3072c0e8356ef8bee0fb361937ca6
Jiselle Liu <jiselle03@gmail.com>
2020-01-27 10:50:48 -0800

    Render answer author info

--- rails/rails_projects/awesome_answers/app/controllers/api/v1/questions_controller.rb
+++ rails/rails_projects/awesome_answers/app/controllers/api/v1/questions_controller.rb
@@ -6,6 +6,6 @@ class Api::V1::QuestionsController < ApplicationController
 
     def show
         question = Question.find(params[:id])
-        render json: question
+        render json: question, include: ['answers.author']
     end
 end
--- rails/rails_projects/awesome_answers/app/serializers/question_serializer.rb
+++ rails/rails_projects/awesome_answers/app/serializers/question_serializer.rb
@@ -25,6 +25,7 @@ class QuestionSerializer < ActiveModel::Serializer
   # serializer within the current serializer.
   class AnswerSerializer < ActiveModel::Serializer
     attributes :id, :body, :created_at, :updated_at
+    belongs_to(:user, key: :author)
   end
 
 end
