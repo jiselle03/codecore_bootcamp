36481858f55d0246720c6c89e932200f92c3373e
Jiselle Liu <jiselle03@gmail.com>
2020-01-27 14:05:40 -0800

    Add private method for product params and create

--- rails/rails_projects/amazing_amazon/app/controllers/api/v1/products_controller.rb
+++ rails/rails_projects/amazing_amazon/app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,17 @@
-class Api::V1::ProductsController < ApplicationController
+class Api::V1::ProductsController < Api::ApplicationController
+    def create
+        product = Product.new product_params
+        product.user = current_user
+        if product.save
+            render json: { id: product.id }
+        else
+            render(
+                json: { errors: product.errors },
+                status: 422 #unproceesable entity
+            )
+        end
+    end
+    
     def index
         products = Product.order(created_at: :desc)
         render json: products
@@ -8,4 +21,11 @@ class Api::V1::ProductsController < ApplicationController
         product = Product.find(params[:id])
         render json: product
     end
+
+    private
+
+    def product_params
+        params.require(:question).permit(:title, :description, :price)
+    end
+
 end
