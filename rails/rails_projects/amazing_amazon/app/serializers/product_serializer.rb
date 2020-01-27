1a581ab9146b680d9f299febdcd78b9957ce5285
Jiselle Liu <jiselle03@gmail.com>
2020-01-27 13:48:42 -0800

    Set up product serializer

--- rails/rails_projects/amazing_amazon/app/serializers/product_serializer.rb
+++ rails/rails_projects/amazing_amazon/app/serializers/product_serializer.rb
@@ -1,3 +1,16 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :description, :price, :created_at, :updated_at, :favorite_count
+
+  belongs_to :user, key: :seller
+  has_many :reviews
+
+  def favorite_count
+    object.favorites.count
+  end
+
+  class ReviewSerializer < ActiveModel::Serializer
+    attributes :id, :body, :created_at, :updated_at
+    belongs_to(:user, key: :author)
+  end
+
 end
