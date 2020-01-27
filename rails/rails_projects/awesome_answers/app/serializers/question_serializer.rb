55b2bd7164cdcf11b39f4fe95cc19dc1d64e8de4
Jiselle Liu <jiselle03@gmail.com>
2020-01-27 10:48:04 -0800

    Add like count method and attribute

--- rails/rails_projects/awesome_answers/app/serializers/question_serializer.rb
+++ rails/rails_projects/awesome_answers/app/serializers/question_serializer.rb
@@ -3,7 +3,7 @@ class QuestionSerializer < ActiveModel::Serializer
   # to include in the serialization output. All columns of a model
   # have a corresponding attr_reader (getter) method, so we can filter
   # this way as well.
-  attributes :id, :title, :body, :created_at, :updated_at, :view_count
+  attributes :id, :title, :body, :created_at, :updated_at, :view_count, :like_count
 
   # To include associated models, use the same named methods for creating
   # associations. You can rename the association with the argument 'key:'
@@ -12,6 +12,17 @@ class QuestionSerializer < ActiveModel::Serializer
   belongs_to :user, key: :author
   has_many :answers
 
+  # You can create methods inside the serializer to include custom data
+  # in the json serialization. When doing so, make sure to include the
+  # name of the method in the 'attributes' call.
+  def like_count
+    # object will refer to the instance of the model being serialized.
+    # Use it where you would use 'self' in the model class.
+    object.likes.count
+  end
+
+  # To customizer a serializer for associated models you can define a
+  # serializer within the current serializer.
   class AnswerSerializer < ActiveModel::Serializer
     attributes :id, :body, :created_at, :updated_at
   end
