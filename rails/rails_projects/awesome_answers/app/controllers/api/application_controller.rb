b5856dc5d2657bb066396ef54455d38312b372b4
Jiselle Liu <jiselle03@gmail.com>
2020-01-27 12:19:05 -0800

    Add authenticate user method

--- rails/rails_projects/awesome_answers/app/controllers/api/application_controller.rb
+++ rails/rails_projects/awesome_answers/app/controllers/api/application_controller.rb
@@ -6,4 +6,13 @@ class Api::ApplicationController < ApplicationController
     # This prevents third parties from making such requests to our Rails app.
     # It's a security measure that is unneccessary in the context of a Web API.
     skip_before_action(:verify_authenticity_token)
+
+    private
+
+    def authenticate_user!
+        unless current_user.present?
+            render json: { status: 401 }, status: 401
+        end
+    end
+
 end
