0803773c27c6583efb6dacb6b31f4fd2c0713f83
Jiselle Liu <jiselle03@gmail.com>
2020-01-29 14:39:53 -0800

    Install Rack CORS gem and config

--- rails/rails_projects/amazing_amazon/config/application.rb
+++ rails/rails_projects/amazing_amazon/config/application.rb
@@ -37,5 +37,16 @@ module AmazingAmazon
       g.helper = false
       g.helper = false
     end
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "localhost:5500", "127.0.0.1:5500", "127.0.0.1"
+        resource '/api/*', 
+        headers: :any,
+        credentials: true,
+        methods: [:get, :post, :delete, :patch, :put, :options]
+      end
+    end
+
   end
 end
