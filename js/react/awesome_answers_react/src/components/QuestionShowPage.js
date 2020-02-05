e032d195b7e139bde2b482717cbcffc5455da312
Jiselle Liu <jiselle03@gmail.com>
2020-02-05 09:33:33 -0800

    Add demo for using state

--- js/react/awesome_answers_react/src/components/QuestionShowPage.js
+++ js/react/awesome_answers_react/src/components/QuestionShowPage.js
@@ -9,17 +9,20 @@ import oneQuestionData from "../oneQuestionData";
 // Components whose names do not begin with a capital letter will be interpreted as a plain HTML tag.
 
 class QuestionShowPage extends Component {
+  constructor(props) {
+    // When using a constructor in a class-based component, you must
+    // call the 'Component' class constructor with 'super' pass it the 'props'.
+    super(props);
+    this.state = {
+      question: oneQuestionData
+    };
+  };
+
   render() {
     return (
       <div className="Page">
-        <QuestionDetails
-          title="What is your favourite color?"
-          body="Red, Green, Magenta, etc."
-          author={{ full_name: "Michael Owen" }}
-          view_count={100}
-          created_at={new Date()}
-        />
-        <AnswerList answers={oneQuestionData.answers} />
+        <QuestionDetails {...this.state.question} />
+        <AnswerList answers={this.state.question.answers} />
       </div>
     );
   };
