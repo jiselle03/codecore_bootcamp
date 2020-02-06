cc75fd51256c1b904b113b985c0f01f5fa941f90
Jiselle Liu <jiselle03@gmail.com>
2020-02-06 13:19:24 -0800

    Add ProductNewPage with form

---
+++ js/react/amazing_amazon_react/src/components/ProductNewPage.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Product } from "../api/product";
+
+export class ProductNewPage extends Component {
+    createQuestion = event => {
+        event.preventDefault();
+        const { currentTarget } = event;
+        const fd = new FormData(currentTarget);
+
+        const newProduct = {
+            title: fd.get("title"),
+            description: fd.get("description"),
+            price: fd.get("price")
+        };
+
+        Product.create(newProduct).then(data => {
+            if (!data.errors) {
+                this.props.history.push(`/products/${data.id}`);
+            };
+        });
+        
+        currentTarget.reset();
+    };
+
+    render() {
+        return (
+            <form 
+                className="NewProductForm ui form" 
+                onSubmit={event => this.createProduct(event)}
+            >
+                <div className="field">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" id="title" />
+                </div>
+                <div className="field">
+                    <label htmlFor="description">Description</label>
+                    <textarea name="description" id="description" />
+                </div>
+                <div className="field">
+                    <label htmlFor="price">Price</label>
+                    <textarea name="price" id="price" />
+                </div>
+                <button className="ui orange button" type="submit">
+                    Add Product
+                </button>
+            </form>
+        );
+    };
+};
