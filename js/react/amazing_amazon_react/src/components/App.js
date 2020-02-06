76f356720c375d92114d17d538428f5385156547
Jiselle Liu <jiselle03@gmail.com>
2020-02-06 13:10:16 -0800

    Add BrowserRouter, Route, Switch and hacky sign in

--- js/react/amazing_amazon_react/src/components/App.js
+++ js/react/amazing_amazon_react/src/components/App.js
@@ -1,16 +1,44 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import ProductShowPage from "./ProductShowPage";
+import { ProductNewPage } from "./ProductNewPage";
 import { ProductIndexPage } from "./ProductIndexPage";
-import { render } from "react-dom";
 
-const App = () => {
-  return (
-    <div className="ui container">
-      <ProductShowPage />
-      <ProductIndexPage />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null
+    };
+  };
+
+  componentDidMount() {
+    Session.create({
+      email: "noone@winterfell.gov",
+      password: "supersecret"
+    }).then(user => {
+      this.setState({ currentUser: user });
+    });
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="ui container segment">
+          <header>
+            <NavBar />
+          </header>
+          <Switch>
+            <Route exact path="/" component={WelcomePage} />
+            <Route exact path="/products" component={ProductIndexPage} />
+            <Route exact path="/products/new" component={ProductNewPage} />
+            <Route exact path="/products/:id" component={ProductShowPage} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  };
 };
 
 export default App;
