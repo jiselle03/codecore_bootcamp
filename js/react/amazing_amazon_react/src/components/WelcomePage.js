5b9ed8708a0913e0bbba4a4789cd5e723b2dcf51
Jiselle Liu <jiselle03@gmail.com>
2020-02-06 13:19:00 -0800

    Add WelcomePage and NavBar components

---
+++ js/react/amazing_amazon_react/src/components/WelcomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const WelcomePage = (props) => (
+    <div className="Page">
+        <div className="ui header">Amazing Amazon</div>
+        <div className="ui teal segment"> Welcome to Amazing Amazon React</div>
+    </div>
+);
