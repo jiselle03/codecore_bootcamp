6d7ac2ad4f8ed994e617f5eb112e92f40fa093a3
Jiselle Liu <jiselle03@gmail.com>
2020-02-06 10:52:07 -0800

    Install react-router and route our page components

--- js/react/awesome_answers_react/src/components/App.js
+++ js/react/awesome_answers_react/src/components/App.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import QuestionShowPage from "./QuestionShowPage";
 import { QuestionIndexPage } from "./QuestionIndexPage";
+import { WelcomePage } from "./WelcomePage";
 import { Clock } from "./Clock";
 
 const App = () => {
   return (
-    <div className="ui container">
-      <Clock />
-      <QuestionIndexPage />
-      <QuestionShowPage />
-    </div>
+    <BrowserRouter>
+      <div className="ui container segment">
+        <Clock />
+        <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/questions" component={QuestionIndexPage} />
+        <Route exact path="/questions/:id" component={QuestionShowPage} />
+      </div>
+    </BrowserRouter>
   );
 };
 
