4ac7c56cca3825d494f4a7e4c9c230efea8976cd
Jiselle Liu <jiselle03@gmail.com>
2020-02-10 14:35:37 -0800

    useState and useEffect for ProductIndexPage

--- js/react/amazing_amazon_react/src/components/ProductIndexPage.js
+++ js/react/amazing_amazon_react/src/components/ProductIndexPage.js
@@ -1,16 +1,20 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
 import { Product } from "../api/product";
 import { Spinner } from "./Spinner";
 
-export class ProductIndexPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-      isLoading: true
-    };
-  };
+export const ProductIndexPage = () => {
+  const [productIndex, setProductIndex] = useState({
+    products: [],
+    isLoading: true
+  });
+
+  useEffect(() => {
+    Product.all().then(products => { 
+      setProductIndex({ products, isLoading: false })
+    });
+  }, []);
 
   // deleteProduct = id => {
   //   Product.destroy(id).then(data => {
@@ -18,40 +22,34 @@ export class ProductIndexPage extends Component {
   //   });
   // };
 
-  componentDidMount() {
-    Product.all().then(products => this.setState({ products: products, isLoading: false }));
-  };
-
-  render() {
-    if(this.state.isLoading) {
-      return(
-        <Spinner message="Loading products..." />
-      );
-    };
-
-    return (
-      <main>
-        <h2
-          style={{margin:"1em"}} 
-          className="ui horizontal divider header">Products</h2>
-        <div className="ui list">
-          {this.state.products.map(product => (
-            <div
-              className="item"
-              key={product.id}>
-            <h3 className="ui header"><Link to={`/products/${product.id}`} className="ui link" href="">
-              {product.title}
-            </Link></h3>
-              {product.description}<br />
-              ${product.price}
-              {/* <button
-                className="ui small right floated red button"
-                onClick={() => this.deleteProduct(product.id)}
-              >Delete</button> */}
-            </div>
-          ))}
-        </div>
-      </main>
+  if(productIndex.isLoading) {
+    return(
+      <Spinner message="Loading products..." />
     );
   };
+
+  return (
+    <main>
+      <h2
+        style={{margin:"1em"}} 
+        className="ui horizontal divider header">Products</h2>
+      <div className="ui list">
+        {productIndex.products.map(product => (
+          <div
+            className="item"
+            key={product.id}>
+          <h3 className="ui header"><Link to={`/products/${product.id}`} className="ui link" href="">
+            {product.title}
+          </Link></h3>
+            {product.description}<br />
+            ${product.price}
+            {/* <button
+              className="ui small right floated red button"
+              onClick={() => this.deleteProduct(product.id)}
+            >Delete</button> */}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 };
