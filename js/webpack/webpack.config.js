f34faf49d75aa89d6f7e473de522e836dd5457a6
Jiselle Liu <jiselle03@gmail.com>
2020-02-11 10:40:02 -0800

    Install HTMLWebpackPlugin to create webpage

--- js/webpack/webpack.config.js
+++ js/webpack/webpack.config.js
@@ -1,3 +1,37 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    mode: "development"
+    // You can specify the environment mode to production or development
+    mode: "development",
+    // You can specify a path to an entry file in multiple ways.
+    // An entry is the file where webpack begins bundling all of your modules.
+    // For a single entry, write: entry: "./src/index.js"
+    // For multiple entry files bundled into a single file, use an array:
+    // entry: ["./src/index.js", "./src/client.js", "./src/default.js"]
+
+    // To create multiple bundles from separate entry files, use an object
+    entry: "./src/index.js",
+    // entry: {
+        // Create a bundle "./dist/client.js" (will be named after the below key)
+        // client: "./src/client.js",
+        // Create a bundle "./dist/defaults.js"
+    //     defaults: "./src/defaults.js",
+    //     server: "./src/index.js"
+    // },
+    output: {
+        // The 'path' property is used to specify the location where our bundled
+        // files are created. By default, it is path.join(__dirname, "build")
+        path: path.join(__dirname, "build"),
+        // The 'filename' property is used to change the names of the bundled files.
+        filename: "[name].bundle.js"
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: "Webpack Demo", // <title> of webpage
+            // chunks: ["main.bundle.js"]
+            // 'chunks' tells webpack which bundles to include in the html file
+        })
+    ]
 }
