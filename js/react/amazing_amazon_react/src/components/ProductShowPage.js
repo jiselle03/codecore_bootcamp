4a5a8b5ee2ad554cfe9fc894b0f8dbe1097030e5
Jiselle Liu <jiselle03@gmail.com>
2020-02-10 16:02:56 -0800

    useEffect and useReducer for ProductShowPage

--- js/react/amazing_amazon_react/src/components/ProductShowPage.js
+++ js/react/amazing_amazon_react/src/components/ProductShowPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useReducer } from "react";
 import { Link } from "react-router-dom";
 
 import "./css/ProductShowPage.css"
@@ -7,76 +7,88 @@ import { ReviewList } from "./ReviewList";
 import { Product } from "../api/product";
 import { Spinner } from "./Spinner";
 
-class ProductShowPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: null,
-      isLoading: true
-    }
-  };
+const initialState = {
+  product: null,
+  isLoading: true
+};
 
-  deleteProduct = id => {
-    Product.destroy(this.state.product.id).then(data => {
-      this.props.history.push("/products");
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_PRODUCT':
+      return {
+        product: action.payload,
+        isLoading: false
+      };
+    
+    case 'DELETE_PRODUCT':
+      return {
+        product: null,
+        isLoading: true
+      };
+    
+    case 'DELETE_REVIEW':
+      return {
+        ...state,
+        product: action.payload
+      };
+  };
+};
+
+const ProductShowPage = props => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const currentProductId = props.match.params.id;
+
+  const deleteProduct = () => {
+    Product.destroy(state.product.id).then(data => {
+      dispatch({ type: 'DELETE_PRODUCT' });
+      props.history.push("/products");
     });
   };
 
-  editProduct = (id, params) => {
-    Product.update(id, params).then(data => {
-      Product.one(this.props.match.params.id).then(product => {
-        this.setState({ product, isLoading: false });
-      });
-    });
-  };  
-
-  deleteReview = id => {
-    const { product } = this.state;
-    this.setState({
-      product: {
-        ...product,
-        reviews: product.reviews.filter(r => r.id !== id)
-      }
-    });
-  };
-
-  componentDidMount() {
-    Product.one(this.props.match.params.id).then(product => {
-      this.setState({ product, isLoading: false });
-    });
-  };
-
-  render() {
-    const { product } = this.state;
-    if(!product) {
-      return(
-        <Spinner message="Product does not exist!" />
-      );
+  const deleteReview = id => {
+    const newReviews = state.product.reviews.filter(r => r.id !== id);
+    const payload = {
+      ...state.product,
+      reviews: newReviews
     };
 
-    return (
-      <div 
-        style={{margin:"0 3em"}}
-        className="Page">
-        <ProductDetails 
-          {...product}
-        />
-        <button
-          style={{margin:"0 3em"}}
-          className="ui small right floated red button"
-          onClick={() => this.deleteProduct()}>Delete</button>
-        
-        <Link exact to={`/products/${product.id}/edit`}><button
-          className="ui small right floated orange button"
-          >Edit</button></Link>
-        <br /><br />
-        <ReviewList 
-        reviews={product.reviews}
-        onReviewDeleteClick={id => this.deleteReview(id)}
-        />
-      </div>
+    dispatch({ type: 'DELETE_REVIEW', payload });
+  };
+
+  useEffect(() => {
+    Product.one(currentProductId).then(product => {
+      dispatch({ type: 'FETCH_PRODUCT', payload: product })
+    });
+  }, [currentProductId]);
+
+  if(state.isLoading) {
+    return(
+      <Spinner message="Product does not exist!" />
     );
   };
+
+  return (
+    <div 
+      style={{margin:"0 3em"}}
+      className="Page">
+      <ProductDetails 
+        {...state.product}
+      />
+      <button
+        style={{margin:"0 3em"}}
+        className="ui small right floated red button"
+        onClick={() => deleteProduct()}>Delete</button>
+      
+      <Link exact to={`/products/${state.product.id}/edit`}><button
+        className="ui small right floated orange button"
+        >Edit</button></Link>
+      <br /><br />
+      <ReviewList 
+      reviews={state.product.reviews}
+      onReviewDeleteClick={id => deleteReview(id)}
+      />
+    </div>
+  );
 
 };
 
