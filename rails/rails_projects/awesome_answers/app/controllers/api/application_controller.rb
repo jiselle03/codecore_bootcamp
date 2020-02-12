5085be68d22d8bc8649c7838e9383882c6574b92
Jiselle Liu <jiselle03@gmail.com>
2020-02-12 10:14:36 -0800

    Handle all API validation errors in a generic way

--- rails/rails_projects/awesome_answers/app/controllers/api/application_controller.rb
+++ rails/rails_projects/awesome_answers/app/controllers/api/application_controller.rb
@@ -7,12 +7,35 @@ class Api::ApplicationController < ApplicationController
     # It's a security measure that is unneccessary in the context of a Web API.
     skip_before_action(:verify_authenticity_token)
 
+    # rescue_from(<error-class>, with: <method-name>)
+    # Use 'rescue_from' in a controller to prevent an error of class <error-class>
+    # from crashing your Rails application and do something else instead.
+    # The <method-name> will handle the error and do whatever you need instead of crashing.
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
     private
 
     def authenticate_user!
         unless current_user.present?
             render json: { status: 401 }, status: 401
         end
+    end
+
+    def record_invalid error
+        invalid_record = error.record
+        errors = invalid_record.errors.map do |field, message|
+            {
+                type: error.class.to_s,
+                record_type: record.class.to_s,
+                field: field,
+                message: message
+            }
+        end
+
+        render(
+            json: { status: 422, errors: errors },
+            status: 422
+        )
     end
 
 end
