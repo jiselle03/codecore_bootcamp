e0a0d6c6a866bace4b2b5bd8f6a902f6d6fbbc47
Jiselle Liu <jiselle03@gmail.com>
2020-02-14 09:40:56 -0800

    Set up active_storage and added avatar attachments for users

--- rails/rails_projects/awesome_answers/app/models/user.rb
+++ rails/rails_projects/awesome_answers/app/models/user.rb
@@ -16,6 +16,16 @@ class User < ApplicationRecord
     has_many :sent_gifts, class_name: "Gift", foreign_key: :sender_id, dependent: :nullify
     has_many :received_gifts, class_name: "Gift", foreign_key: :receiver_id, dependent: :nullify
 
+    # To set up attachments or uploads, generate a migraiton to create tables needed by ActiveStorage:
+    # rails active_storage:install
+    # Then run your migration
+    
+    # To support multiple file attachments, do:
+    # has_many_attached :avatars
+
+    # To support a single file attachment, do:
+    has_one_attached :avatar    
+
     validates :email, presence: true, uniqueness: true,
     format: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     has_secure_password
